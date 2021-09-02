c41a75321344c84410a6692f10137c3b0b17cd83
Wpeng <72253291+Wpenga@users.noreply.github.com>
2021-09-02 20:19:42 +0800

    安装portainer
    汉化并更新

--- portainer.sh
+++ portainer.sh
@@ -1,1 +1,2 @@
+echo '安装portainer镜像并汉化'
 wget https://hub.fastgit.org/Wpenga/myself/releases/download/portainer-cn/public.zip && unzip public.zip && docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/public:/public portainer/portainer
